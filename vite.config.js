fe5d65325776a7f81463a5af9768e8222bc048fd
Eugeniya_South <paladina.em1@kvt.su>
2025-11-25 14:27:00 +0300

    Инициализация проекта

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import { resolve } from 'path'
+import createSvgSpritePlugin from 'vite-plugin-svg-spriter'
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+const FRONT_PATH = 'src';
+
+export default defineConfig({
+    root: 'src',
+    plugins: [
+        createSvgSpritePlugin({
+            svgFolder: resolve(__dirname,`${FRONT_PATH}/assets/images/svg`),
+        }),
+        ViteImageOptimizer({
+            jpg: {
+                quality: 75
+            },
+            png: {
+                quality: 75
+            },
+            jpeg: {
+                quality: 75
+            }
+        })
+    ],    
+    build: {
+        minify: true,
+        cssMinify: true,
+        rolldownOptions: {
+            input: {
+                index: resolve(__dirname, `${FRONT_PATH}/index.html`),
+                catalog: resolve(__dirname, `${FRONT_PATH}/html/catalog/index.html`),
+                about: resolve(__dirname, `${FRONT_PATH}/html/about/index.html`),
+            }
+        }
+    }
+});
